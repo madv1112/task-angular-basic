d2cab8061f918e9ffbcdecff6db23ba77fe64de6
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-26 15:52:15 -0300

    adding moving task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Primeiro test!';
-  task: Task = new Task(1605, 'Gerenciando arquivos para o cliente!');
-}
 
-export class Task{
+  title = 'Gerenciador de Tarefas';
 
-  public id: number;
-  public title: string;
-
-  constructor(id: number, title: string){
-    this.id = id;
-    this.title = title
-  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
-const TASK: Array<any> = [
+import { Task } from "./shared/task.model";
+
+const TASK: Array<Task> = [
     {id: 1, title: 'Tarefa número 1'},
-    {id: 2, title: 'Tarefa número 2'},
+    {id: 2, title: 'Tarefa número 2 e verificando...'},
     {id: 3, title: 'Tarefa número 3'},
     {id: 4, title: 'Tarefa número 4'},
     {id: 5, title: 'Tarefa número 5'},
-    {id: 6, title: 'Tarefa número 6'}
+    {id: 6, title: 'Tarefa número 6 e finalizando...'}
 ]
 
 @Component({
@@ -16,7 +18,7 @@ const TASK: Array<any> = [
 
 export class TaskComponent implements OnInit{
 
-    task;
+    public task;
 
     ngOnInit(){
         this.task = TASK;
