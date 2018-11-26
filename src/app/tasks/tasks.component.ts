3feaab2045fa63de3438087d37fc90a9ce0a60b2
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-26 17:47:52 -0300

    adding task detail model

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -5,7 +5,7 @@ import { Task } from "./shared/task.model";
 const TASK: Array<Task> = [
     {id: 1, title: 'Tarefa número 1'},
     {id: 2, title: 'Tarefa número 2 e verificando...'},
-    {id: 3, title: 'Tarefa número 3'},
+    {id: 3, title: 'Mais uma tarefa iniciada com sucesso!'},
     {id: 4, title: 'Tarefa número 4'},
     {id: 5, title: 'Tarefa número 5'},
     {id: 6, title: 'Tarefa número 6 e finalizando...'}
@@ -18,9 +18,14 @@ const TASK: Array<Task> = [
 
 export class TaskComponent implements OnInit{
 
-    public task;
+    public tasks;
+    public selectedTask: Task;
 
     ngOnInit(){
-        this.task = TASK;
+        this.tasks = TASK;
+    }
+
+    onSelect(task: Task): void{
+        this.selectedTask = task;
     }
 }
