33622df3125bc4cb53086cc144ff7e4fdfa0056c
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-28 12:02:15 -0300

    learning-binding

---
+++ src/app/learning-binding/learning-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'learning-binding',
+    templateUrl: 'learning-binding.component.html'
+})
+
+export class LearningBindingComponent{
+
+}
