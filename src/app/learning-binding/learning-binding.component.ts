09e22119006c2cd318075bf29319a5d533b6f41a
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-28 16:29:44 -0300

    adding binding logic to view

--- src/app/learning-binding/learning-binding.component.ts
+++ src/app/learning-binding/learning-binding.component.ts
@@ -7,11 +7,24 @@ import { Component } from "@angular/core";
 
 export class LearningBindingComponent{
 
+    public userName: string;
+    public userEmail: string;
+
+    constructor(){
+        this.userName = "User nome";
+        this.userEmail = "user email"
+    }
+
     public onClick(){
         console.log('Evento do click disparado!');
     }
 
     public onMouseOver(){
         console.log('Evento do mouse disparado!');
+    }
+
+    public onKeyUp(event: any){
+        console.log();
+        this.userName = event.target.value;
     }
 }
