0f130d64093cea466207dd8fd28e581a85c3f560
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-29 15:56:25 -0300

    adding task-detail

---
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+import { TaskComponent } from './../tasks.component';
+
+import { Task } from './../shared/task.model';
+
+@Component({
+    selector: 'task-detail',
+    templateUrl: './task-detail.component.html'
+})
+
+export class TaskDetailComponent{
+
+    @Input() public task: Task;
+
+    constructor(){}
+}
