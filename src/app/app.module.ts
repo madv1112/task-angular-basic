73a2160fbdf7b1cf6faa43d63d0df531b05c1cdb
madv1112 <36240172+madv1112@users.noreply.github.com>
2018-12-12 16:28:28 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
-import { TaskmanagerFrontendPage } from './../../e2e/app.po';
-import { Task } from './tasks/shared/task.model';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
